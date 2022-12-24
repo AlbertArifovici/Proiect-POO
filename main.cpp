5dc09cb2d69ee0af58d540635c1dffecd50bbe0b
albertarifovici <albertarifovici@yahoo.com>
2022-12-24 22:19:21 +0200

    Adaugare atribute si metode specifice clasei

--- main.cpp
+++ main.cpp
@@ -7,70 +7,70 @@ using namespace std;
 
 class Ferma
 {
-    unsigned int nrAnimale;
+    unsigned int nrHambare;
     unsigned int nrUtilajeInLucru;
     unsigned int nrUtilajeInStationare;
 public:
 
-    Ferma(int nrAnimale, int nrUtilajeInLucru)
+    Ferma(int nrHambare, int nrUtilajeInLucru)
     {
-        this->nrAnimale = nrAnimale;
+        this->nrHambare = nrHambare;
         this->nrUtilajeInLucru = nrUtilajeInLucru;
     }
 
-    Ferma(int nrAnimale, int nrUtilajeInLucru, int nrUtilajeInStationare, int *preturiPiese)
+    Ferma(int nrHambare, int nrUtilajeInLucru, int nrUtilajeInStationare, int *nrAnimaleInHambare)
     {
-        this->nrAnimale = nrAnimale;
+        this->nrHambare = nrHambare;
         this->nrUtilajeInLucru = nrUtilajeInLucru;
         this->nrUtilajeInStationare = nrUtilajeInStationare;
-        this->setPreturiPiese(nrAnimale, preturiPiese);
+        this->setNrAnimaleInHambare(nrHambare, nrAnimaleInHambare);
     }
 
-    void setPreturiPiese(int nrAnimale, int* preturiPiese)
+    void setNrAnimaleInHambare(int nrHambare, int* nrAnimaleInHambare)
     {
-        this->preturiPiese = new int[nrAnimale];
-        for (int i = 0; i < nrAnimale; i++)
-            this->preturiPiese[i] = preturiPiese[i];
+        this->nrAnimaleInHambare = new int[nrHambare];
+        for (int i = 0; i < nrHambare; i++)
+            this->nrAnimaleInHambare[i] = nrAnimaleInHambare[i];
     }
 
     //destructor
     ~Ferma()
     {
-        if (this->preturiPiese != NULL)
-            delete[] preturiPiese;
+        if (this->nrAnimaleInHambare != NULL)
+            delete[] nrAnimaleInHambare;
     }
 
     //constructor copiere
     Ferma(const Ferma& s)
     {
-        this->nrAnimale = s.nrAnimale;
+        this->nrHambare = s.nrHambare;
         this->nrUtilajeInLucru = s.nrUtilajeInLucru;
         this->nrUtilajeInStationare = s.nrUtilajeInStationare;
-        for (int i = 0; i < this->getNrAnimale(); i++)
+        for (int i = 0; i < this->getnrHambare(); i++)
         {
-            this->preturiPiese[i] = s.preturiPiese[i];
+            this->nrAnimaleInHambare[i] = s.nrAnimaleInHambare[i];
         }
     }
 
     //operator atribuire
     Ferma operator=(const Ferma& s)
     {
-        this->nrAnimale = s.nrAnimale;
+        this->nrHambare = s.nrHambare;
         this->nrUtilajeInLucru = s.nrUtilajeInLucru;
         this->nrUtilajeInStationare = s.nrUtilajeInStationare;
-        delete[]preturiPiese;
-        this->preturiPiese = new int[s.nrAnimale];
-        for (int i = 0; i < s.nrAnimale; i++)
+        delete[]nrAnimaleInHambare;
+        this->nrAnimaleInHambare = new int[s.nrHambare];
+        for (int i = 0; i < s.nrHambare; i++)
         {
-            this->preturiPiese[i] = s.preturiPiese[i];
+            this->nrAnimaleInHambare[i] = s.nrAnimaleInHambare[i];
         }
         return *this;
     }
 
     //getters
-    int getNrAnimale()
+    int getnrHambare()
     {
-        return this->nrAnimale;
+        return this->nrHambare;
     }
 
     int getnrUtilajeInStationare()
@@ -86,78 +86,68 @@ public:
     //operator iesire
     friend ostream& operator<< (ostream& out, const Ferma& s)
     {
-        out<<"Nr Piese: " << s.nrAnimale<<endl;
-        out<<"Nr masini in service: " << s.nrUtilajeInLucru<<endl;
-        out<<"Nr Masini Reparate: "  << s.nrUtilajeInStationare<<endl;
-        cout << "Preturi piese: "<<endl;
-        for (int i = 0; i < s.nrAnimale; i++)
-            out << s.preturiPiese[i]<<" ";
+        out<<"Nr hambare: " << s.nrHambare<<endl;
+        out<<"Nr utilaje in lucru: " << s.nrUtilajeInLucru<<endl;
+        out<<"Nr utilaje in stationare: "  << s.nrUtilajeInStationare<<endl;
+        cout<<endl;
+        for (int i = 0; i < s.nrHambare; i++)
+            out << "Nr animale in hambarul " << i << ": " << s.nrAnimaleInHambare[i]<<endl;
         return out;
     }
 
     //operator intrare
     friend istream& operator>> (istream& in, Ferma& s)
     {
-        in >> s.nrAnimale;
+        in >> s.nrHambare;
         in >> s.nrUtilajeInLucru;
         in >> s.nrUtilajeInStationare;
-        for (int i = 0; i < s.getNrAnimale(); i++)
+        for (int i = 0; i < s.getnrHambare(); i++)
         {
-            in >> s.preturiPiese[i];
+            in >> s.nrAnimaleInHambare[i];
         }
         return in;
     }
 
     bool operator== (Ferma& s)
     {
-        if (this->nrAnimale != s.nrAnimale || this->nrUtilajeInStationare != s.nrUtilajeInStationare || this->nrUtilajeInLucru != s.nrUtilajeInLucru || this->preturiPiese != s.preturiPiese)
+        if (this->nrHambare != s.nrHambare || this->nrUtilajeInStationare != s.nrUtilajeInStationare || this->nrUtilajeInLucru != s.nrUtilajeInLucru || this->nrAnimaleInHambare != s.nrAnimaleInHambare)
             return false;
         else
             return true;
     }
 
-    int * preturiPiese;
+    int *nrAnimaleInHambare;
 };
 
-
-
-float media(Ferma& s)
+//metode specifice
+float mediaAnimale(Ferma& s)
 {
     float media;
     float suma = 0;
-    int n = s.getNrAnimale();
+    int n = s.getnrHambare();
     for (int i = 0; i < n; i++)
-        suma += s.preturiPiese[i];
+        suma += s.nrAnimaleInHambare[i];
     return suma / n;
 }
 
 
-void sortare(Ferma& s)
+void sortareHambare(Ferma& s)
 {
-    int n = s.getNrAnimale();
+    int n = s.getnrHambare();
     for (int i = 0; i < n; i++)
-        for (int j = i + 1; j < n - 1; j++)
-            if (s.preturiPiese[i] > s.preturiPiese[j])
-                swap(s.preturiPiese[i], s.preturiPiese[j]);
+        for (int j = i + 1; j < n ; j++)
+            if (s.nrAnimaleInHambare[i] > s.nrAnimaleInHambare[j])
+                swap(s.nrAnimaleInHambare[i], s.nrAnimaleInHambare[j]);
 
 }
 
-
-
 int main()
 {
-    Ferma s1(5, 3, 2, new int [5] {6, 7, 8, 9, 10});
+    Ferma s1(5, 3, 2, new int [5] {10,9,8,7,6});
     cout<<s1<<endl;
-    //Service s(5, 3);
-    //cout<<s.getnrAnimale();
-    /*
-	//Service s1(5, 3, 2, new int [5] {6, 7, 8, 9, 10});
-	//cout << s1;
-	//Service s4;
-	//cin >> s4;
-	cout<<(s==s1);
-	sortare(s);
-	media(s);
-     */
+    cout<<"Media animalelor per hambar: "<<mediaAnimale(s1)<<endl;
+    cout<<endl;
+    sortareHambare(s1);
+    cout<<s1<<endl;
     return 0;
 }
